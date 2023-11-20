43b1d7f31617ec399f23c9b588bdf145f9957868
Eka <eka.suryana@unj.ac.id>
2023-11-20 17:21:15 +0700

    partial_p29

--- solver_p29.d
+++ solver_p29.d
@@ -4,6 +4,18 @@ import io;
 
 void solve_sort_indexes(int[] data){
     //TODO: Solve the solution
-    print!int.printArray(data);
+    //print!int.printArray(data);
+    int[] indices = new int[data.length];
+    for(int i=0;i<data.length;i++)
+        indices[i] = i;
+    
+    //prepare double loop
+    for(int i=0;i<data.length;i++){
+        for(int j=0;j<data.length;j++){
+            if(data[j] > data[j+1]){
+                //Todo: swap data
+            }
+        }
+    }
 
 }
